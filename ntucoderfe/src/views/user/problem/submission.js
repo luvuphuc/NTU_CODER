be81d9e08267329bbfc26ddeac73ae6c1d7290de
luvuphuc <luvuphuc@gmail.com>
2025-02-26 18:50:09 +0700

    Edit submission

--- ntucoderfe/src/views/user/problem/submission.js
+++ ntucoderfe/src/views/user/problem/submission.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Box, Container, Spinner, Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+import { motion } from "framer-motion";
 import Split from "react-split";
 import Header from "../common/header";
 import Navigation from "../common/navigation";
@@ -8,8 +9,10 @@ import FooterUser from "../common/footer";
 import api from "utils/api";
 import ProblemTab from "../components/problem_tab";
 import RankingTab from "../components/ranking_tab";
-import DiscussionTab from "../components/discussting_tab";
+import DiscussionTab from "../components/discussion_tab";
 import EditorTab from "../components/editor_tab";
+
+const MotionTab = motion(Tab);
 
 const Submission = () => {
   const { id } = useParams();
@@ -38,28 +41,93 @@ const Submission = () => {
       <Navigation />
 
       <Container maxW="container.xl" py={3} flex="1" bg="white" borderRadius="lg" boxShadow="lg" my={6}>
-        {/* Sử dụng Split để chia phần Tabs (40%) và Editor (60%) */}
+        {/* Chia phần Tabs (40%) và Editor (60%) */}
         <Split className="split" sizes={[40, 60]} minSize={300} gutterSize={10} direction="horizontal">
           {/* Tabs: Bài tập, Xếp hạng, Thảo luận */}
-          <Box width="40%">
-            <Tabs variant="soft-rounded" colorScheme="purple">
-              <TabList>
-                <Tab>Bài tập</Tab>
-                <Tab>Xếp hạng</Tab>
-                <Tab>Thảo luận</Tab>
+          <Box width="40%" position="relative">
+            <Tabs variant="unstyled">
+              <TabList
+                position="absolute"
+                top="-3"
+                left="-4"
+                width="103%"
+                borderBottom="1px solid #C0C0C0"
+                bg="#E3E3E3"
+                display="flex"
+                gap={2}
+                padding="8px 10px"
+                zIndex={1}
+                borderTopRadius="lg"
+              >
+                <MotionTab
+                  _selected={{
+                    bg: "white",
+                    borderBottom: "2px solid #E3E3E3",
+                    fontWeight: "bold",
+                  }}
+                  _hover={{ background: "#DADADA" }}
+                  borderTopRadius="lg"
+                  px={4}
+                  py={2}
+                  whileHover={{ scale: 1.05 }}
+                  whileTap={{ scale: 0.95 }}
+                  transition={{ duration: 0.2 }}
+                >
+                  Bài tập
+                </MotionTab>
+                <MotionTab
+                  _selected={{
+                    bg: "white",
+                    borderBottom: "2px solid #E3E3E3",
+                    fontWeight: "bold",
+                  }}
+                  _hover={{ background: "#DADADA" }}
+                  borderTopRadius="lg"
+                  px={4}
+                  py={2}
+                  whileHover={{ scale: 1.05 }}
+                  whileTap={{ scale: 0.95 }}
+                  transition={{ duration: 0.2 }}
+                >
+                  Xếp hạng
+                </MotionTab>
+                <MotionTab
+                  _selected={{
+                    bg: "white",
+                    borderBottom: "2px solid #E3E3E3",
+                    fontWeight: "bold",
+                  }}
+                  _hover={{ background: "#DADADA" }}
+                  borderTopRadius="lg"
+                  px={4}
+                  py={2}
+                  whileHover={{ scale: 1.05 }}
+                  whileTap={{ scale: 0.95 }}
+                  transition={{ duration: 0.2 }}
+                >
+                  Thảo luận
+                </MotionTab>
               </TabList>
 
-              <TabPanels>
-                <TabPanel>
-                  <ProblemTab problem={problem} />
-                </TabPanel>
-                <TabPanel>
-                  <RankingTab />
-                </TabPanel>
-                <TabPanel>
-                  <DiscussionTab />
-                </TabPanel>
-              </TabPanels>
+              {/* Tạo khoảng trống phía trên nội dung để tránh bị che mất bởi TabList */}
+              <Box pt="50px">
+                <TabPanels
+                  as={motion.div}
+                  initial={{ opacity: 0, y: 10 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 0.3 }}
+                >
+                  <TabPanel>
+                    <ProblemTab problem={problem} />
+                  </TabPanel>
+                  <TabPanel>
+                    <RankingTab />
+                  </TabPanel>
+                  <TabPanel>
+                    <DiscussionTab />
+                  </TabPanel>
+                </TabPanels>
+              </Box>
             </Tabs>
           </Box>
 
