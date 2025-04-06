43b3118ce248cec05d185930a854b5b2ecdbaa87
luvuphuc <luvuphuc@gmail.com>
2025-04-06 10:35:01 +0700

    CRUDs for Participation

---
+++ ntucoderbe/Controllers/ParticipationController.cs
@@ -0,0 +1,133 @@
+﻿using AddressManagementSystem.Infrashtructure.Helpers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ntucoderbe.DTOs;
+using ntucoderbe.Infrashtructure.Repositories;
+using ntucoderbe.Infrashtructure.Services;
+
+namespace ntucoderbe.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ParticipationController : ControllerBase
+    {
+        private readonly ParticipationRepository _repository;
+        private readonly AuthService _authService;
+
+        public ParticipationController(ParticipationRepository repository, AuthService authService)
+        {
+            _repository = repository;
+            _authService = authService;
+        }
+
+        [HttpGet("all")]
+        public async Task<IActionResult> GetAllParticipations([FromQuery] QueryObject query, string? sortField = null, bool ascending = true)
+        {
+            try
+            {
+                var result = await _repository.GetAllParticipationsAsync(query, sortField, ascending);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> CreateParticipation([FromBody] ParticipationDTO dto)
+        {
+            if (dto == null)
+            {
+                return BadRequest(new { Errors = new List<string> { "Dữ liệu không hợp lệ." } });
+            }
+
+            try
+            {
+                var coderID = _authService.GetUserIdFromToken();
+                dto.CoderID = coderID;
+                var result = await _repository.CreateParticipationAsync(dto);
+                return CreatedAtAction(nameof(GetParticipationById), new { id = result.ParticipationID }, result);
+            }
+            catch (InvalidOperationException ex)
+            {
+                return BadRequest(new { Errors = new List<string> { ex.Message } });
+            }
+        }
+
+        // Get problem by ID
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetParticipationById(int id)
+        {
+            try
+            {
+                var problem = await _repository.GetParticipationByIdAsync(id);
+
+                if (problem == null)
+                {
+                    return NotFound(new { Message = "Không tìm thấy vấn đề với ID được cung cấp." });
+                }
+
+                return Ok(problem);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateParticipation(int id, [FromBody] ParticipationDTO dto)
+        {
+            if (dto == null)
+            {
+                return BadRequest(new { Errors = new List<string> { "Dữ liệu không hợp lệ." } });
+            }
+
+            try
+            {
+                var result = await _repository.UpdateParticipationAsync(id, dto);
+                return Ok(result);
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(new { Message = ex.Message });
+            }
+            catch (InvalidOperationException ex)
+            {
+                return BadRequest(new { Errors = new List<string> { ex.Message } });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        // Delete a problem by ID
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSubmission(int id)
+        {
+            try
+            {
+                var isDeleted = await _repository.DeleteParticipationAsync(id);
+
+                if (isDeleted)
+                {
+                    return Ok();
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new
+                {
+                    Message = "Có lỗi xảy ra.",
+                    Error = ex.Message
+                });
+            }
+        }
+    }
+}
---
+++ ntucoderbe/Infrashtructure/Repositories/ParticipationRepository.cs
@@ -0,0 +1,140 @@
+﻿using AddressManagementSystem.Infrashtructure.Helpers;
+using Microsoft.EntityFrameworkCore;
+using ntucoderbe.DTOs;
+using ntucoderbe.Models;
+using ntucoderbe.Models.ERD;
+
+namespace ntucoderbe.Infrashtructure.Repositories
+{
+    public class ParticipationRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ParticipationRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<PagedResponse<ParticipationDTO>> GetAllParticipationsAsync(QueryObject query, string? sortField = null, bool ascending = true)
+        {
+            var objQuery = _context.Participations
+                .Include(c => c.Contest)
+                .Include(c=>c.Coder)
+               .Select(c => new ParticipationDTO
+               {
+                   ParticipationID = c.ParticipationID,
+                   ContestID = c.ContestID,
+                   ContestName = c.Contest.ContestName,
+                   CoderID = c.CoderID,
+                   CoderName = c.Coder.CoderName,
+                   Rank = c.Rank,
+                   TimeScore = c.TimeScore,
+                   PointScore  = c.PointScore,
+
+               });
+            objQuery = ApplySorting(objQuery, sortField, ascending);
+            var obj = await PagedResponse<ParticipationDTO>.CreateAsync(
+                objQuery,
+                query.Page,
+                query.PageSize);
+            return obj;
+        }
+
+        public IQueryable<ParticipationDTO> ApplySorting(IQueryable<ParticipationDTO> query, string? sortField, bool ascending)
+        {
+            return sortField?.ToLower() switch
+            {
+                "rank" => ascending ? query.OrderBy(a => a.Rank) : query.OrderByDescending(a => a.Rank),
+                _ => query.OrderBy(a => a.ParticipationID)
+            };
+        }
+
+        public async Task<ParticipationDTO> CreateParticipationAsync(ParticipationDTO dto)
+        {
+            if (await IsParticipationExistAsync(dto.CoderID, dto.ContestID))
+            {
+                throw new InvalidOperationException("Đã đăng ký là thành viên.");
+            }
+            var par = new Participation
+            {
+                ContestID = dto.ContestID,
+                CoderID = dto.CoderID,
+                RegisterTime = dto.RegisterTime,
+                PointScore = 0,
+                TimeScore = 0,
+                Rank = 0,
+                SolvedCount = 0,
+                RegisterMAC = dto.RegisterMAC,
+            };
+
+            _context.Participations.Add(par);
+            await _context.SaveChangesAsync();
+            return dto;
+        }
+        public async Task<bool> IsParticipationExistAsync(int coderID, int contestID)
+        {
+            return await _context.Participations.AnyAsync(c => c.CoderID == coderID && c.ContestID ==contestID);
+        }
+
+        public async Task<ParticipationDTO> GetParticipationByIdAsync(int id)
+        {
+            var obj = await _context.Participations.FirstOrDefaultAsync(c => c.ParticipationID == id);
+            if (obj == null)
+            {
+                throw new KeyNotFoundException("Không tìm thấy thể loại.");
+            }
+
+            return new ParticipationDTO
+            {
+                ParticipationID = obj.ParticipationID,
+                ContestID = obj.ContestID,
+                ContestName = obj.Contest.ContestName,
+                CoderID = obj.CoderID,
+                CoderName = obj.Coder.CoderName,
+                Rank = obj.Rank,
+                TimeScore = obj.TimeScore,
+                PointScore = obj.PointScore,
+            };
+        }
+
+        public async Task<ParticipationDTO> UpdateParticipationAsync(int id, ParticipationDTO dto)
+        {
+            var obj = await _context.Participations.FirstOrDefaultAsync(c => c.ParticipationID == id);
+            if (obj == null)
+            {
+                throw new KeyNotFoundException("Không tìm thấy thể loại.");
+            }
+
+            obj.TimeScore = dto.TimeScore ?? obj.TimeScore;
+            obj.PointScore = dto.PointScore ?? obj.PointScore;
+            obj.Rank = dto.Rank ?? obj.Rank;
+
+            _context.Participations.Update(obj);
+            await _context.SaveChangesAsync();
+
+            return new ParticipationDTO
+            {
+                ParticipationID = obj.ParticipationID,
+                ContestID = obj.ContestID,
+                ContestName = obj.Contest.ContestName,
+                CoderID = obj.CoderID,
+                CoderName = obj.Coder.CoderName,
+                Rank = obj.Rank,
+                TimeScore = obj.TimeScore,
+                PointScore = obj.PointScore,
+            };
+        }
+
+        public async Task<bool> DeleteParticipationAsync(int id)
+        {
+            var obj = await _context.Participations.FirstOrDefaultAsync(c => c.ParticipationID == id);
+            if (obj == null)
+            {
+                return false;
+            }
+            _context.Participations.Remove(obj);
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+    }
+}
